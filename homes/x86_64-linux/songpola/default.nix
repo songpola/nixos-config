7704bb95ffa256a50bf7fe298e43b84cac56547b
Songpol Anannetikul <1527535+songpola@users.noreply.github.com>
2024-12-13 09:19:17 +0000

    refactor: :recycle: modularize config

--- homes/x86_64-linux/songpola/default.nix
+++ homes/x86_64-linux/songpola/default.nix
@@ -1,7 +1,7 @@
 {
   # # Snowfall Lib provides a customized `lib` instance with access to your flake's library
   # # as well as the libraries available from your flake's inputs.
-  # lib,
+  lib,
   # # An instance of `pkgs` with your overlays and packages applied is also available.
   # pkgs,
   # # You also have access to your flake's inputs.
@@ -16,5 +16,5 @@
   # # All other arguments come from the home home.
   # config,
   ...
-}: {
-}
+} @ args:
+lib.songpola.mkModuleFrom ./. args
--- lib/module/default.nix
+++ lib/module/default.nix
@@ -9,21 +9,29 @@
   # # Additionally, Snowfall Lib's own inputs are passed. You probably don't need to use this!
   # snowfall-inputs,
 }: let
-  inherit (lib) mapAttrsToList haumea;
+  inherit (lib) attrValues haumea pathExists;
 in rec {
   mkImportsFrom = path:
-    mapAttrsToList (name: value: value)
-    (
+    if pathExists path
+    then
+      attrValues
+      (
+        haumea.load {
+          src = path;
+          loader = haumea.loaders.path;
+        }
+      )
+    else [];
+
+  mkConfigFrom = path: specialArgs:
+    if pathExists path
+    then
       haumea.load {
         src = path;
-        loader = haumea.loaders.path;
+        inputs = specialArgs;
       }
-    );
-  mkConfigFrom = path: specialArgs:
-    haumea.load {
-      src = path;
-      inputs = specialArgs;
-    };
+    else {};
+
   mkModuleFrom = path: specialArgs: {
     imports = mkImportsFrom (path + "/imports");
     config = mkConfigFrom (path + "/config") specialArgs;
