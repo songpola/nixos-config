718f3a154ee22ef2c015bfa0fe4788e19c810272
Songpol Anannetikul <1527535+songpola@users.noreply.github.com>
2024-12-14 01:38:03 +0000

    feat: :sparkles: add git config

---
+++ homes/x86_64-linux/songpola/config/programs/git.nix
@@ -0,0 +1,9 @@
+{
+  enable = true;
+  userEmail = "1527535+songpola@users.noreply.github.com";
+  userName = "Songpol Anannetikul";
+  extraConfig = {
+    init.defaultBranch = "main";
+  };
+  delta.enable = true;
+}
