3658f1381c88df7ba7c6a7a3568dbf3fad495c6f
Songpol Anannetikul <1527535+songpola@users.noreply.github.com>
2024-12-21 11:21:34 +0000

    feat(doctor): :sparkles: add ssh-wrapper package and module

---
+++ packages/ssh-wrapper/default.nix
@@ -0,0 +1,36 @@
+{
+  # Snowfall Lib provides a customized `lib` instance with access to your flake's library
+  # as well as the libraries available from your flake's inputs.
+  # lib,
+  # You also have access to your flake's inputs.
+  # inputs,
+  # The namespace used for your flake, defaulting to "internal" if not set.
+  # namespace,
+  # All other arguments come from NixPkgs. You can use `pkgs` to pull packages or helpers
+  # programmatically or you may add the named attributes as arguments here.
+  pkgs,
+  # stdenv,
+  ...
+}:
+with pkgs;
+  symlinkJoin {
+    name = builtins.baseNameOf ./.;
+    paths = [
+      openssh
+      # songpola.ssh-win
+    ];
+    postBuild = ''
+      # ssh
+      mv $out/bin/ssh $out/bin/ssh-wsl
+      # ln -s $out/bin/ssh-win $out/bin/ssh
+      ln -s /mnt/c/Windows/System32/OpenSSH/ssh.exe $out/bin/ssh
+
+      # ssh-add
+      mv $out/bin/ssh-add $out/bin/ssh-add-wsl
+      ln -s /mnt/c/Windows/System32/OpenSSH/ssh-add.exe $out/bin/ssh-add
+
+      # ssh-keygen
+      mv $out/bin/ssh-keygen $out/bin/ssh-keygen-wsl
+      ln -s /mnt/c/Windows/System32/OpenSSH/ssh-keygen.exe $out/bin/ssh-keygen
+    '';
+  }
