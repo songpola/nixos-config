202d7470685002d85fc7a305d879607b66f83b8a
Songpol Anannetikul <1527535+songpola@users.noreply.github.com>
2025-01-31 23:31:42 +0000

    feat(prts): :sparkles: add qemu vm config

---
+++ systems/x86_64-linux/prts/vm/qemu/disk-config.nix
@@ -0,0 +1,124 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        device = "/dev/disk/by-id/ata-QEMU_HARDDISK_QM00001";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "1G";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/efi";
+                mountOptions = ["umask=0077"];
+              };
+            };
+            root = {
+              end = "-8G";
+              content = {
+                type = "btrfs";
+                # extraArgs = ["-f"]; # Override existing partition
+                subvolumes = {
+                  "@" = {
+                    mountOptions = ["compress=zstd"];
+                    mountpoint = "/";
+                  };
+                  "@home" = {
+                    mountOptions = ["compress=zstd"];
+                    mountpoint = "/home";
+                  };
+                  "@nix" = {
+                    mountOptions = ["compress=zstd" "noatime"];
+                    mountpoint = "/nix";
+                  };
+                };
+              };
+            };
+            swap.end = "-4G"; # zramSwap backing device
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "tank";
+              };
+            };
+          };
+        };
+      };
+      ssd = {
+        device = "/dev/disk/by-id/ata-QEMU_HARDDISK_QM00003";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "tank";
+              };
+            };
+          };
+        };
+      };
+      hdd = {
+        device = "/dev/disk/by-id/ata-QEMU_HARDDISK_QM00005";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "tank";
+              };
+            };
+          };
+        };
+      };
+    };
+    zpool = {
+      tank = {
+        type = "zpool";
+        mode = {
+          topology = {
+            type = "topology";
+            vdev = [
+              {
+                members = ["hdd"];
+              }
+            ];
+            log = [
+              {
+                members = ["main"];
+              }
+            ];
+            special = [
+              {
+                members = ["ssd"];
+              }
+            ];
+          };
+        };
+        # rootFsOptions = {
+        #   compression = "zstd";
+        #   # "com.sun:auto-snapshot" = "false";
+        # };
+        # mountpoint = "/mnt/tank";
+        # datasets = {
+        #   # See examples/zfs.nix for more comprehensive usage.
+        #   test = {
+        #     type = "zfs_fs";
+        #     mountpoint = "/mnt/tank/test";
+        #     # options."com.sun:auto-snapshot" = "true";
+        #   };
+        # };
+      };
+    };
+  };
+}
