84adf582248f0e2baa95f1e6efe884a53b93ad1a
Songpol Anannetikul <1527535+songpola@users.noreply.github.com>
2025-02-01 03:37:50 +0000

    fix(prts): :bug: use systemd-networkd instead

--- systems/x86_64-linux/prts/networking.nix
+++ systems/x86_64-linux/prts/networking.nix
@@ -1,15 +1,31 @@
 {
-  # systemd.network.enable = true;
-  # networking = {
-  #   useDHCP = false;
-  #   bridges = {
-  #     # br0.interfaces = ["eno1"];
-  #     br0.interfaces = ["enp1s0"];
-  #   };
-  #   interfaces = {
-  #     br0.useDHCP = true;
-  #     # eno1.useDHCP = true;
-  #     enp1s0.useDHCP = true;
-  #   };
-  # };
+  networking.useDHCP = false;
+  systemd.network = {
+    enable = true;
+    netdevs = {
+      # Create the bridge interface
+      "20-br0" = {
+        netdevConfig = {
+          Name = "br0";
+          Kind = "bridge";
+        };
+      };
+    };
+    networks = {
+      # Connect the bridge ports to the bridge
+      "30-ens33" = {
+        matchConfig.Name = "ens33"; # vm
+        networkConfig.Bridge = "br0";
+        linkConfig.RequiredForOnline = "enslaved";
+      };
+      # Configure the bridge for its desired function
+      "40-br0" = {
+        matchConfig.Name = "br0";
+        # bridgeConfig = {};
+        networkConfig.DHCP = "yes";
+        # networkConfig.LinkLocalAddressing = "no"; # Disable address autoconfig when no IP configuration is required
+        linkConfig.RequiredForOnline = "carrier"; # or "routable" with IP addresses configured
+      };
+    };
+  };
 }
