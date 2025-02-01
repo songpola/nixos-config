3aac1090bdcb0ac01e3e9219d745176d0a4cdfa3
Songpol Anannetikul <1527535+songpola@users.noreply.github.com>
2025-02-01 11:54:32 +0000

    refactor(prts): :recycle: remove unused route

--- systems/x86_64-linux/prts/default.nix
+++ systems/x86_64-linux/prts/default.nix
@@ -56,7 +56,7 @@ in {
       openFirewall = true;
       useRoutingFeatures = "server";
       extraSetFlags = [
-        "--advertise-routes=10.0.0.0/16,192.168.100.0/24"
+        "--advertise-routes=10.0.0.0/16"
         "--advertise-exit-node"
         "--operator=songpola"
         "--ssh"
