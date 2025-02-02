e12873c47173503aea8a4c90e6333eb94de914c6
Songpol Anannetikul <1527535+songpola@users.noreply.github.com>
2025-02-02 08:43:11 +0000

    refactor: :recycle: remove unused deploy-rs

--- flake.nix
+++ flake.nix
@@ -26,11 +26,6 @@
       url = "github:snowfallorg/flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    deploy-rs = {
-      url = "github:serokell/deploy-rs";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = inputs:
@@ -55,17 +50,5 @@
           nixos-wsl.nixosModules.default
         ];
       };
-    }
-    // {
-      deploy.nodes.prts = {
-        hostname = "192.168.146.128";
-        profiles.system = with inputs; {
-          user = "root";
-          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.prts;
-        };
-      };
-
-      # This is highly advised, and will prevent many possible mistakes
-      checks = with inputs; builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
     };
 }
