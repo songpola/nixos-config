26d12d750de3d26824905895804fe0a60062cc57
Songpol Anannetikul <1527535+songpola@users.noreply.github.com>
2025-02-03 11:32:15 +0000

    feat: :sparkles: add disko scope

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,13 +2,13 @@
 const fs = require("node:fs")
 const path = require("node:path")
 
-let packages = fs.readdirSync(path.resolve(__dirname, "systems/x86_64-linux"))
-
-// Ignore default.template.nix
-packages = packages.filter((pkg) => pkg != "default.template.nix")
+const systems = fs
+  .readdirSync(path.resolve(__dirname, "systems/x86_64-linux"))
+  .filter((pkg) => pkg != "default.template.nix") // Ignore default.template.nix
+const disko = fs.readdirSync(path.resolve(__dirname, "disko"))
 
 module.exports = {
   prompt: {
-    scopes: [...packages],
+    scopes: [...systems, ...disko],
   },
 }
