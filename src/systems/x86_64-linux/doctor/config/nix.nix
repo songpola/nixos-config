8c652e20ebfbc59816415362a2ed271c9e345ad8
Songpol Anannetikul <1527535+songpola@users.noreply.github.com>
2025-02-28 22:56:27 +0700

    feat(doctor): :sparkles: use prts as binary cache

---
+++ src/systems/x86_64-linux/doctor/config/nix.nix
@@ -0,0 +1,10 @@
+{
+  settings = {
+    substituters = [
+      "http://prts:5000/"
+    ];
+    trusted-public-keys = [
+      "prts-1:SDYZAeLQIUUHEasfmPNUFtnFgW+q2LiwX0+dOvPerbk="
+    ];
+  };
+}
