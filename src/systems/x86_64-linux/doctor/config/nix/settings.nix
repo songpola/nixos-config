2e2a8a0d47806f080d146e3cfe04782865a5dff6
Songpol Anannetikul <1527535+songpola@users.noreply.github.com>
2025-03-05 17:46:36 +0700

    fix(doctor): :bug: use FQDN for prts substituter

--- src/systems/x86_64-linux/doctor/config/nix/settings.nix
+++ src/systems/x86_64-linux/doctor/config/nix/settings.nix
@@ -3,6 +3,6 @@
   _lib,
 }: {
   builders-use-substitutes = true;
-  substituters = ["http://prts:5000/"];
+  substituters = ["http://prts.tail7623c.ts.net:5000/"];
   trusted-public-keys = [_lib.public.harmonia];
 }
