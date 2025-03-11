59368500a7284c9a931bbeb2d15adffa1e3bb5bf
Songpol Anannetikul <1527535+songpola@users.noreply.github.com>
2025-03-12 00:08:37 +0700

    style: :lipstick: format code

--- src/systems/x86_64-linux/doctor/config/nix/settings.nix
+++ src/systems/x86_64-linux/doctor/config/nix/settings.nix
@@ -3,6 +3,10 @@
   _lib,
 }: {
   builders-use-substitutes = true;
-  substituters = ["http://prts.tail7623c.ts.net:5000/"];
-  trusted-public-keys = [_lib.public.harmonia];
+  substituters = [
+    "http://prts.tail7623c.ts.net:5000/"
+  ];
+  trusted-public-keys = [
+    _lib.public.harmonia
+  ];
 }
