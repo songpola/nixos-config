e448c682bfe98945a7c970e75f37e0ccf3025a71
Songpol Anannetikul <1527535+songpola@users.noreply.github.com>
2025-04-15 23:40:17 +0700

    refactor: :recycle: change commit scopes

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -5,6 +5,6 @@ const systems = fs.readdirSync(path.resolve(__dirname, "systems/x86_64-linux"))
 
 module.exports = {
   prompt: {
-    scopes: [...systems, "just", "shell", "modules"],
+    scopes: [...systems, "modules"],
   },
 }
