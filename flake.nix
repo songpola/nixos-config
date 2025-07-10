10cd668069c683f923f1b6a4574adb98d9ab8466
Songpol Anannetikul <1527535+songpola@users.noreply.github.com>
2025-07-10 06:18:51 +0000

    feat: :sparkles: add client: doctor

--- flake.nix
+++ flake.nix
@@ -1,10 +1,14 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     nixos-wsl.url = "github:nix-community/NixOS-WSL";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    haumea.url = "github:nix-community/haumea/v0.2.2";
+    haumea.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
@@ -37,6 +41,75 @@
             ./modules/dev/vscode-remote.nix
 
             ./modules/home-manager.nix
+          ];
+        };
+
+        doctor = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            nixos-wsl.nixosModules.default
+
+            # Options
+            (
+              { lib, ... }:
+              let
+                inherit (lib) mkOption types;
+              in
+              {
+                options = {
+                  core = mkOption {
+                    type = types.str;
+                  };
+                  api = mkOption {
+                    type = types.listOf types.str;
+                  };
+                };
+              }
+            )
+
+            # Core: WSL
+            (
+              { lib, config, ... }:
+              let
+                inherit (lib) mkMerge mkIf;
+
+                useCore = core: config.core == core;
+              in
+              mkMerge [
+                (mkIf (useCore "wsl") {
+                  wsl.enable = true;
+                })
+              ]
+            )
+
+            # API: user
+            (
+              { lib, config, ... }:
+              let
+                inherit (lib) mkMerge mkIf;
+
+                useApi = api: builtins.elem api config.api;
+                useCore = core: config.core == core;
+              in
+              mkMerge [
+                (mkIf (useApi "user") (mkMerge [
+                  (mkIf (useCore "wsl") {
+                    # If WSL
+                    wsl.defaultUser = "songpola";
+                  })
+                ]))
+              ]
+            )
+
+            # Client: doctor
+            {
+              core = "wsl";
+              api = [
+                "user"
+              ];
+
+              system.stateVersion = "24.11";
+            }
           ];
         };
       };
