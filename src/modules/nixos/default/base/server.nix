c46a50c6d45921b18310638bac30e78e46f8db22
Songpol Anannetikul <1527535+songpola@users.noreply.github.com>
2025-07-11 11:32:05 +0700

    fix: :bug: add trusted-users

--- src/modules/nixos/default/base/server.nix
+++ src/modules/nixos/default/base/server.nix
@@ -11,4 +11,9 @@ lib.${namespace}.mkBaseModule config "server" {
   # Remote access
   services.openssh.enable = true;
   users.users.${namespace}.openssh.authorizedKeys.keys = [ lib.${namespace}.sshPublicKey ];
+
+  nix.settings = {
+    # To prevent the `error: cannot ... because it lacks a signature by a trusted key`
+    trusted-users = [ namespace ];
+  };
 }
