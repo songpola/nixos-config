86347226fca6c79758da5c23b0c926debc8fd455
Songpol Anannetikul <1527535+songpola@users.noreply.github.com>
2025-07-19 18:32:24 +0700

    refactor(starship): edit prompt settings

--- src/modules/nixos/default/presets/tools/starship.nix
+++ src/modules/nixos/default/presets/tools/starship.nix
@@ -9,10 +9,50 @@ let
   inherit (lib) mkIf;
 
   presets = [
+    "catppuccin-powerline"
     "nerd-font-symbols"
   ];
   settings = {
-    os.disabled = false; # Enable showing current OS symbol
+    # Enable line breaks for two-line prompts.
+    # `disabled = true` by default from catppuccin-powerline preset.
+    line_break.disabled = false;
+    # Enable showing current OS symbol
+    os.disabled = false;
+    # Enable showing current shell symbol
+    shell.disabled = false;
+
+    # Add $shell to the prompt
+    format =
+      ''
+        [](red)
+        $os
+        $username
+        [](bg:peach fg:red)
+        $directory
+        [](bg:yellow fg:peach)
+        $git_branch
+        $git_status
+        [](fg:yellow bg:green)
+        $c
+        $rust
+        $golang
+        $nodejs
+        $php
+        $java
+        $kotlin
+        $haskell
+        $python
+        [](fg:green bg:sapphire)
+        $conda
+        [](fg:sapphire bg:lavender)
+        $time
+        [ ](fg:lavender)
+        $cmd_duration
+        $line_break
+        $shell
+        $character
+      ''
+      |> lib.replaceString "\n" ""; # remove newlines
   };
 
   package = pkgs.starship;
