5a85348be4b17c8b2385adc999ba88ae655fbf60
Songpol Anannetikul <1527535+songpola@users.noreply.github.com>
2025-07-20 22:52:40 +0700

    feat: add init-wsl package as WSL setup helper

---
+++ src/packages/init-wsl/default.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  pkgs,
+  nushell,
+  writeTextFile,
+  ...
+}:
+let
+  name = builtins.baseNameOf ./.;
+  packages = [ pkgs.nh ];
+in
+writeTextFile {
+  inherit name;
+  executable = true;
+  destination = "/bin/${name}";
+  allowSubstitutes = true;
+  preferLocalBuild = false;
+  text =
+    ''
+      #!${lib.getExe nushell}
+
+      use std/util "path add"
+    ''
+    + (packages |> map (package: "path add ${package}/bin") |> lib.concatLines)
+    + ''
+
+      def --wrapped main [hostname: string, ...rest] {
+        (
+          nh os boot github:songpola/nixos-config
+            --hostname $hostname
+            ...$rest
+            --
+            --extra-experimental-features 'nix-command flakes pipe-operators'
+        )
+      }
+    '';
+}
