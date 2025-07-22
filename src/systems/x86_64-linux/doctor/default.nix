c93573ead6d32f1355531c07a28892f338cdc635
Songpol Anannetikul <1527535+songpola@users.noreply.github.com>
2025-07-22 09:16:43 +0700

    refactor(containers/trilium): remove container
    Move into separate repo and use Compose instead.

--- src/systems/x86_64-linux/doctor/default.nix
+++ src/systems/x86_64-linux/doctor/default.nix
@@ -1,12 +1,10 @@
 {
-  config,
   lib,
   namespace,
   ...
 }:
 let
   inherit (lib) mkMerge;
-  inherit (lib.${namespace}) mkHomeConfigModule mkRootlessQuadletModule;
 in
 mkMerge [
   {
@@ -23,24 +21,4 @@ mkMerge [
       };
     };
   }
-  (mkRootlessQuadletModule config (quadletCfg: {
-    containers = {
-      trilium = {
-        serviceConfig = {
-          Restart = "on-failure";
-        };
-        containerConfig = {
-          image = "docker.io/triliumnext/trilium:v0.96.0";
-          publishPorts = [ "8080:8080" ];
-          volumes = [
-            "${quadletCfg.volumes.trilium-data.ref}:/home/node/trilium-data"
-            "/etc/localtime:/etc/localtime:ro"
-          ];
-        };
-      };
-    };
-    volumes = {
-      trilium-data = { };
-    };
-  }))
 ]
