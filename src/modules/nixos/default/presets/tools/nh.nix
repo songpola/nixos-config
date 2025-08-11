7558abdcff20d5f52462570c8be9c56b23d6bef5
Songpol Anannetikul <1527535+songpola@users.noreply.github.com>
2025-08-11 14:34:40 +0700

    fix(nh): only perform clean on WSL hosts

--- src/modules/nixos/default/presets/tools/nh.nix
+++ src/modules/nixos/default/presets/tools/nh.nix
@@ -5,34 +5,20 @@
   ...
 }:
 lib.${namespace}.mkPresetModule config [ "tools" "nh" ] {
-  systemConfig = [
-    {
-      # nh - Yet another Nix CLI helper
-      # https://github.com/nix-community/nh
-      #
-      # NOTE: No need to use the options from Home Manager,
-      # because all the profiles (system and user) will be cleaned by this options.
-      programs.nh = {
-        enable = true;
-        clean.enable = true; # auto clean (all) (default: weekly)
-        flake = lib.${namespace}.nixosConfigPath;
-      };
-    }
-  ];
   extraConfig = [
-    (lib.${namespace}.mkIfBaseEnabled config "server" {
+    (lib.${namespace}.mkIfBaseEnabled config "wsl" {
       systemConfig = [
         {
-          # Servers would have plenty of storage.
-          # So we can keep the cache for a long time.
-          programs.nh.clean.extraArgs =
-            [
-              "--keep=10" # keep at least 10 generations
-              "--keep-since=1m" # keep gcroots and generations in the last 1 month
-              "--nogc" # don't run `nix store --gc`
-              "--nogcroots" # don't clean gcroots
-            ]
-            |> lib.concatStringsSep " ";
+          # nh - Yet another Nix CLI helper
+          # https://github.com/nix-community/nh
+          #
+          # NOTE: No need to use the options from Home Manager,
+          # because all the profiles (system and user) will be cleaned by this options.
+          programs.nh = {
+            enable = true;
+            clean.enable = true; # auto clean (all) (default: weekly)
+            flake = lib.${namespace}.nixosConfigPath;
+          };
         }
       ];
     })
