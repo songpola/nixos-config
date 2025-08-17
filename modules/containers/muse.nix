043daa97e9547af00087d7caf71923849c81f768
Songpol Anannetikul <1527535+songpola@users.noreply.github.com>
2025-08-17 13:14:10 +0700

    feat(containers): add muse container (Kay-chan Discord Bot)

---
+++ modules/containers/muse.nix
@@ -0,0 +1,39 @@
+{
+  delib,
+  homeconfig,
+  host,
+  username,
+  secrets,
+  ...
+}:
+let
+  name = "muse";
+  secretName = "containers/${name}/env";
+in
+delib.module rec {
+  inherit name;
+
+  options = delib.singleEnableOption host.containersFeatured;
+
+  myconfig.ifEnabled.secrets.enable = true;
+
+  nixos.ifEnabled.sops.secrets.${secretName}.owner = username;
+
+  home.ifEnabled = delib.rootlessQuadletModule homeconfig { } {
+    containers.${name} = {
+      serviceConfig.Restart = "on-failure";
+      containerConfig = {
+        # Fix for v2.11.1
+        # See: https://github.com/museofficial/muse/pull/1256
+        image = "ghcr.io/museofficial/muse:pr-1256";
+        volumes = [
+          "/tank/songpola/muse/data:/data"
+        ];
+        environments = {
+          ENABLE_SPONSORBLOCK = "true";
+        };
+        environmentFiles = [ secrets.${secretName}.path ];
+      };
+    };
+  };
+}
